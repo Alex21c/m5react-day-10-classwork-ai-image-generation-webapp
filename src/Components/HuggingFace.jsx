6d798169830ef00a0b78c9fb3cf3e0d771d0d911
Abhishek kumar <aasalex21c@gmail.com>
2024-03-30 14:56:08 +0530

    switched api key

--- src/Components/HuggingFace.jsx
+++ src/Components/HuggingFace.jsx
@@ -5,10 +5,10 @@ import SuccessAndErrorMsg from "./SuccessAndErrorMsg";
 import userEvent from "@testing-library/user-event";
 
 
-// const HF_TOKEN1 = process.env.REACT_APP_HF_TOKEN1;
-const HF_TOKEN2 = process.env.REACT_APP_HF_TOKEN2;
+const HF_TOKEN1 = process.env.REACT_APP_HF_TOKEN1;
+// const HF_TOKEN2 = process.env.REACT_APP_HF_TOKEN2;
 
-const inference = new HfInference(HF_TOKEN2);
+const inference = new HfInference(HF_TOKEN1);
 
 
 export default function HuggingFace(){  
